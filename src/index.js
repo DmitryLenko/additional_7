e6f1e15e066ed1b86c9e89306d38640cadaed1a0
DmitryLenko <dias1381254@gmail.com>
2018-09-17 18:38:59 +0300

    added checking for rows of columns and squares

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    for(let row = 0; row<matrix.length; row++) {
+      for(let col = 0; col < matrix[row].length; col++) {
+        if(matrix[row][col] === 0) {
+          for (let number = 1; number <= 9; number++) {
+            if (checkInRow(number, row, matrix)  &&  checkInColumn(number, col, matrix)  &&  CheckInBox(matrix , col, row, number)) {
+              console.log(row,col,number)
+              console.log('----------------------')
+              matrix[row][col] = number;
+              
+            }
+          }  
+        };
+      };
+    };   
+    console.log(matrix);
+};
+
+function checkInRow(number, row, matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[row][i] === number) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkInColumn(number, col, matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][col] === number) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function CheckInBox(matrix , col, row, number) {
+  var r = row - row % 3;
+		var c = col - col % 3;
+		for (let i = r; i < r + 3; i++) {
+      for (let j = c; j < c + 3; j++) {
+        if (matrix[i][j] === number) {
+          return false;
+        }
+      }
+    }
+  return true;	
 }
